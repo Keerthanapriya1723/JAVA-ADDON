6a40bddc1aa82fb1d40edd379eed7b7541a0ef71
Keerthanapriya1723 <gkeerthanap@gmail.com>
2025-06-10 14:35:16 +0530

    Add files via upload

---
+++ Day4/Abstraction.java
@@ -0,0 +1,26 @@
+package Day4;
+ 
+//                            Abstract classes (extends)
+ abstract class Student{
+	 abstract void m1();
+ }
+ class display extends Student{
+	 void m1() {
+		 System.out.println("Haii");
+	 }
+ }
+public class Abstraction {
+
+	public static void main(String[] args) {
+		display d=new display();
+		d.m1();
+	}
+
+}
+  
+
+
+
+
+
+
