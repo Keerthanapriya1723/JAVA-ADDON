5d6968aab31fba7658e712419c1c463f4b000d01
Keerthanapriya1723 <gkeerthanap@gmail.com>
2025-06-10 11:53:18 +0530

    Arrays.java

--- Task/Arrays.java
+++ Task/Arrays.java
@@ -1,28 +1,52 @@
 import java.util.Scanner;
+public class arrray {
 
-public class VowelsConsonants {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the String: ");
-        
-        String input = in.nextLine(); 
 
-        int vowel = 0, consonant = 0;
+	public static void main(String[] args) {
 
-        for (int i = 0; i < input.length(); i++) {
-            char ch = Character.toLowerCase(input.charAt(i)); 
+	//	int[] number=new int[5];
 
-            if (Character.isLetter(ch)) {
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                    vowel++;
-                } else {
-                    consonant++;
-                }
-            }
-        }
+	//	number[0]=1;
 
-        System.out.println("Vowels: " + vowel);
-        System.out.println("Consonants: " + consonant);
-    }
-}
+	//	number[1]=2;
+
+	//	number[2]=3;
+
+	//	number[3]=4;
+
+	//	number[4]=5;
+
+		int[] number= {1,2,3,4,5};
+
+	System.out.println(number[3]);
+
+	System.out.println(number.length);
+
+	
+
+		//int sum=0;
+
+	int count=0;
+
+		for(int i=0; i<=number.length-1; i++) {
+
+			//sum=sum+number[i];
+
+		count++;
+
+		System.out.println(number[i]);}
+
+		System.out.println("count is " +count);
+
+		//}
+
+       //reverse number
+
+		//for(int i=number.length-1;i>=0;i--) {
+
+			//System.out.println(number[i]);
+
+		}
+
+	}
